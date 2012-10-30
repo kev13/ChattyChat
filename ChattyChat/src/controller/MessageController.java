052999a0f6b70b96a9c547448978ebd812780a5e
Engelbert Luechinger <eluechin@hsr.ch>
2012-10-30 17:31:22 +0100

    added userlist to chat

--- ChattyChat/src/controller/MessageController.java
+++ ChattyChat/src/controller/MessageController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -9,17 +11,25 @@ import javax.faces.event.ActionEvent;
 
 import model.Chat;
 import model.Message;
+import model.User;
 
 @ManagedBean
 @SessionScoped
 public class MessageController {
 	private Chat chat;
 	private List<Message> history;
+	private Set<User> users;
 	private String content;
+	private String username;
 
 	public MessageController() {
 		chat = new Chat();
 		history = chat.getHistory();
+		users = chat.getUsers();
+		User u = new User();
+		u.setName("ueli");
+		users.add(u);
+		username = u.getName();
 	}
 
 	public void sendMessage(ActionEvent e) {
@@ -32,15 +42,27 @@ public class MessageController {
 	}
 
 	public List<Message> getHistory() {
-		return chat.getHistory();
+		return history;
+	}
+	
+	public List<User> getUsers(){
+		return new ArrayList<User>(users);
 	}
 
 	public String getContent() {
 		return content;
 	}
+	
+	public String getUsername(){
+		return username;
+	}
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+	
+	public void addUser(User u){
+		users.add(u);
 	}
 
 }
